6276db030eed0ae57e241db25f88e33c277821c0
nguyenleuit <24520021@gm.uit.edu.vn>
2026-01-18 09:51:07 +0000

    Add CryptoHack solutions

---
+++ RSA/Inferius/Inferius_Soln.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes, GCD
+
+e = 3
+n = 742449129124467073921545687640895127535705902454369756401331
+ct = 39207274348578481322317340648475596807303160111338236677373  ## this is the cipher text 
+
+p = 752708788837165590355094155871
+q = 986369682585281993933185289261
+
+phi = (p-1)*(q-1)
+
+d = inverse(e,phi)
+
+a = pow(ct,d,n)
+m = long_to_bytes(a)
+print(m)
+
